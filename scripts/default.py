a824241777fca79e3ee3dd3211c642757be565b4
Jacob van't Hoog <vantjac@gmail.com>
2016-12-19 12:55:33 -0800

    Initial commit

---
+++ scripts/default.py
@@ -0,0 +1,42 @@
+
+def makePlayer():
+    forwardButtonAxis = ButtonAxis(world.buttonInputs['w'], 0.0, 1.0)
+    backButtonAxis = ButtonAxis(world.buttonInputs['s'], 0.0, 1.0)
+    leftButtonAxis = ButtonAxis(world.buttonInputs['a'], 0.0, 1.0)
+    rightButtonAxis = ButtonAxis(world.buttonInputs['d'], 0.0, 1.0)
+    
+    xWalkAxis = AxisSum(rightButtonAxis, AxisOpposite(leftButtonAxis))
+    yWalkAxis = AxisSum(forwardButtonAxis, AxisOpposite(backButtonAxis))
+    
+    player = FirstPersonPlayer( world,
+                                AxisScale(world.axisInputs['mouse-x'], .005),
+                                AxisScale(world.axisInputs['mouse-y'], .005),
+                                xWalkAxis, yWalkAxis,
+                                world.buttonInputs['space'],
+                                walkSpeed = 25.0, fallMoveSpeed = 15.0,
+                                jumpVelocity = 50.0)
+    world.camera = player
+    return player
+
+def use():
+    cam = world.camera
+    
+    def getMeshCallback(mesh, face):
+        if mesh is not None:
+            mesh.doUseAction()
+            
+    world.getFaceAtRay( getMeshCallback, cam.getPosition(),
+                        Vector(1.0, 0.0, 0.0).rotate(cam.getRotation()) )
+
+class UseScanner(SimObject):
+
+    def __init__(self, button):
+        self.button = button
+
+    def scan(self, timeElapsed, totalTime):
+        event = self.button.getEvent()
+        if event == ButtonInput.PRESSED_EVENT:
+            use()
+
+world.simulator.addObject(UseScanner(world.buttonInputs['mouse-left']))
+
